f06ee364f0898282ee2649ca3ecf1b31b823b64b
delase1 <bdnterful@gmail.com>
2023-01-18 21:07:50 +0000

    work

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM  from "react-dom";
 import App from './App';
 import { Provider } from "react-redux";
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter  } from 'react-router-dom';
 
 import 'antd/dist/antd.min.css'
 import store from './app/store'
 
 
 
-
-ReactDOM.render(
-<Router>
-    <Provider store={store} >
-        <App />
-    </Provider>
-</Router>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+ <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+ </React.StrictMode>
+);
